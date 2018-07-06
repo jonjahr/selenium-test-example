b949ef26ea74dc3824b53a7c1faa8d8c4d844d3a
Jonathan Jahr <jonjahr@gmail.com>
2018-07-06 11:16:29 -0700

    improve comments

--- examples/test.js
+++ examples/test.js
@@ -1,95 +1,104 @@
-/*
-Selenium Test
-Quick Start:
-  * yarn add selenium-webdriver (This installs Selenium Webdriver: https://www.npmjs.com/package/selenium-webdriver)
-  * Go here and follow instructions to install the Firefox geckodriver, and make sure it's in your console's PATH: https://www.npmjs.com/package/selenium-webdriver
-  * cd path/to/selenium-tests
-  * node ccd-test-01.js
-  * You should see a Firefox window appear, and see the viewport jump around and text get typed in.
-  * You should see your console say "[test name] Starting...", "[test name] Success..."
-*/
-
+// Import the Selenium package
 const { Builder, By, Key, until } = require("selenium-webdriver");
+
+// Create one driver.  We'll run tests in series on one driver (one browser window).
 const driver = new Builder().forBrowser("firefox").build();
+
+// Path to your local development server.  We'll run tests on this page.
 const url = "http://localhost:8000";
+
+// Keep track of what test we're on.
 var testIndex = 0;
+
+/***************************************
+ * ARRAY OF TESTS
+ **************************************/
+
+// Add your tests here.
+
+// PROPERTIES:
+// name: Your test's name (human readable)
+// test: Which function to call (see functions below)
+// el1, el2... : These are the page elements we'll click on, check, etc.
+// All selectors are browser css selectors (some jquery selectors won't work)
+// When checking for text, providing a substring is ok.
 
 const tests = [
   {
     name: "Sector Grid 1",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-0",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 2",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-1",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 3",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-2",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 4",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-3",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 5",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-4",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 6",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-5",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 7",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-6",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 8",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-7",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 9",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-8",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 10",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-9",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 11",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-10",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Sector Grid 12",
-    test: "clickAndCheckVisible",
+    test: "click__checkVisible",
     el1: ".sector-grid-11",
     el2: "#sectorFilterMessage"
   },
   {
     name: "Test ZIP Filter with 95811",
-    test: "typeAndClickAndCheckText",
+    test: "type__click__checkText",
     el1: "#input-sectors-zip",
     el1_text: "95811",
     el2: "#input-sectors-zip-button",
@@ -102,26 +111,26 @@ const tests = [
 runTest(0);
 
 /***************************************
- * The Tests
+ * TEST FUNCTIONS
  **************************************/
 
-function clickAndCheckText(options) {
-  // Click a button (el1), then check for text (in el2)
+// These functions perform common Selenium actions
+// They let us work at a higher level in the "tests" array above.
+
+function click__checkText(options) {
+  // Click a button, then check that an element contains text
   const { name, el1, el2, el2_text } = { ...options };
-  // name: for console logging
   // el1: element to click (css selector)
-  // el2: element to look for text in (css selector)
-  // el2_text: text substring to look for
+  // el2: element to check for text (css selector)
+  // el2_text: text to look for (substring is ok)
   console.log(name, "Starting...");
   driver
     .get(url)
-    // Click Element 1
     .then(_ => click(el1))
     .then(_ => checkIfElementContainsText(el2, el2_text))
     // Catch error
     .catch(err => {
       console.error(name, "Error:", err);
-      // getNextTest();
     })
     // Success
     .then(_ => {
@@ -130,13 +139,13 @@ function clickAndCheckText(options) {
     });
 }
 
-function typeAndClickAndCheckText(options) {
-  // Type text (into el1), click a button (el2), then check for text (in el3).
+function type__click__checkText(options) {
+  // Type text into an element, click a button, then check that an element contains text.
   const { name, el1, el1_text, el2, el3, el3_text } = { ...options };
-  // name: for console logging
   // el1: element to click (css selector)
-  // el2: element to look for text in (css selector)
-  // text: text substring to look for
+  // el1_text
+  // el2: element to check for text (css selector)
+  // text: text to look for (substring is ok)
   console.log(name, "Starting...");
   driver
     .get(url)
@@ -146,7 +155,6 @@ function typeAndClickAndCheckText(options) {
     // Catch error
     .catch(err => {
       console.error(name, "Error:", err);
-      // getNextTest();
     })
     // Success
     .then(_ => {
@@ -155,12 +163,12 @@ function typeAndClickAndCheckText(options) {
     });
 }
 
-function clickAndCheckVisible(options) {
-  // Click a button (el1), then check if element is visible (in el2)
+function click__checkVisible(options) {
+  // Click a button, then check that an element is visible
   const { name, el1, el2 } = { ...options };
   // name: for console logging
   // el1: element to click (css selector)
-  // el2: element to look for (css selector)
+  // el2: element to check for visibility (css selector)
   console.log(name, "Starting...");
   driver
     .get(url)
@@ -183,6 +191,30 @@ function clickAndCheckVisible(options) {
  * Helper Functions
  **************************************/
 
+function runTest(index) {
+  if (index < tests.length) {
+    // Call function based on the current string.
+    const test = tests[index];
+    if (test.test == "click__checkVisible") {
+      click__checkVisible(test);
+    } else if (test.test == "click__checkText") {
+      click__checkText(test);
+    } else if (test.test == "type__click__checkText") {
+      type__click__checkText(test);
+    }
+  } else {
+    // We've reached end of array.  All tests are done.
+    // Close the browser window and the Selenium instance.
+    driver.quit();
+    console.log("Tests done.");
+  }
+}
+
+function getNextTest() {
+  testIndex++;
+  runTest(testIndex);
+}
+
 function typeTextInto(el, text) {
   return driver.findElement(By.css(el)).sendKeys(text);
 }
@@ -203,28 +235,4 @@ function checkIfElementContainsText(el, text) {
     until.elementTextContains(driver.findElement(By.css(el)), text),
     1000
   );
-}
-
-function getNextTest() {
-  testIndex++;
-  runTest(testIndex);
-}
-
-function runTest(index) {
-  if (index >= tests.length) {
-    // var str = driver.toString();
-    console.log("Tests done.");
-    // if (str) {
-    driver.quit();
-    // }
-  } else {
-    const test = tests[index];
-    if (test.test == "clickAndCheckVisible") {
-      clickAndCheckVisible(test);
-    } else if (test.test == "clickAndCheckText") {
-      clickAndCheckText(test);
-    } else if (test.test == "typeAndClickAndCheckText") {
-      typeAndClickAndCheckText(test);
-    }
-  }
 }
